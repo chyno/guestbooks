5ad20e2307e30444020bcf84434101e92ba48c43
Chynoweth, John W <john.w.chynoweth@uscis.dhs.gov>
2019-04-10 11:51:43 -0400

    guest book renders data

--- pages/guestbook.js
+++ pages/guestbook.js
@@ -1,10 +1,28 @@
 import Link from 'next/link'
+import React from 'react'
 
-export default function Guestbook() {
-    return (
-      <div>
-        <p>Hello From guestbook.</p>
-        <Link href="/"><a>Back to Main</a></Link>
-      </div>
-    )
+class Guestbook extends React.Component {
+  static async getInitialProps({ req }) {
+    const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
+    return { userAgent }
   }
+
+  render() {
+    return (
+    <div>
+        <div>Hello World {this.props.userAgent}</div>
+        <div><Link href="/"><a>Back to Main</a></Link></div>
+        </div>);
+  }
+}
+
+export default Guestbook
+
+// export default function Guestbook() {
+//     return (
+//       <div>
+//         <p>Hello From guestbook.</p>
+//         <Link href="/"><a>Back to Main</a></Link>
+//       </div>
+//     )
+//   }
