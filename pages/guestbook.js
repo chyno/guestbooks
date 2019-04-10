2481c48be12316609ad1645fe2cc3fd23d8c8afb
Chynoweth, John W <john.w.chynoweth@uscis.dhs.gov>
2019-04-10 16:11:16 -0400

    iso fetch import

--- pages/guestbook.js
+++ pages/guestbook.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
+import "isomorphic-fetch";
 
 class Guestbook extends React.Component {
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 
 // This is the Link API
 import Link from 'next/link';
-
+import "isomorphic-fetch";
 import {
   Form,
   TextField, SubmitField 
