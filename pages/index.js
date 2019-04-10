bddf0c7b0157949801f7e2a117096c998aaf344c
Chynoweth, John W <john.w.chynoweth@uscis.dhs.gov>
2019-04-10 11:15:45 -0400

    post submit

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,53 @@ import Link from 'next/link'
 import { format } from 'util';
 //import {Message} from './components';
 const Index = () => (
-<form name='valform' method='post'  onSubmit={(e) => {
-  console.log(document.forms[0]);
-  e.preventDefault();      
-  return false;
-}}>
-<div>
-    <label >name</label> <input id='name'></input>
-    <label >message</label> <input id='message'></input>
-    <input type='button' value='Submit' ></input>
-</div>
-  <Message/>
-</form>
+<MainForm/>
 
 )
 
-
 export default Index
+
+
+class MainForm extends React.Component {
+    constructor(props) {
+      
+      super(props);
+      this.state = {name : '', message: ''};
+  
+      this.handleNameChange = this.handleNameChange.bind(this);
+      this.handleMessageChange = this.handleMessageChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleNameChange(event) {
+      this.setState(Object.assign({}, this.state, { name : event.target.value}));
+    }
+
+    handleMessageChange(event) {
+        this.setState(Object.assign({}, this.state, { message : event.target.value}));
+      }
+  
+    handleSubmit(event) {
+      alert('A name was submitted: ' + JSON.stringify(this.state));
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            name:
+            <input type="text" value={this.state.name} onChange={this.handleNameChange} />
+          </label>
+          <label>
+            message:
+            <input type="text" value={this.state.message} onChange={this.handleMessageChange} />
+          </label>
+          <input type="submit" value="Submit" /> 
+        </form>
+      );
+    }
+  }
 
 
 class Message extends React.Component {
