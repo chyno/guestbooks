c4a539c35ef76b0b6a913e971bb3a08f854b59cd
Chynoweth, John W <john.w.chynoweth@uscis.dhs.gov>
2019-04-10 12:31:54 -0400

    add express dependencies

--- server.js
+++ server.js
@@ -5,18 +5,29 @@ const bodyParser = require('body-parser')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const guestbooks = [{foo:'bar'}];
 
 app.prepare().then(() => {
-  const server = express()
+  const server = express();
 
-  server.use(bodyParser.json())
+  server.use(bodyParser.json());
+
+  server.get('/api/guestbooks', (req, res) => {
+    return app.render(req, this.guestbooks, '/api/guestbooks', req.query);
+  })
 
   server.get('*', (req, res) => {
-    return handle(req, res)
-  })
+    return handle(req, res);
+  });
+
+  server.post('/api/guestbook', (req, res) => {
+     console.log(req.body);
+    this.guestbooks.push(req.body);
+    res.send('success');
+  });
 
   server.listen(3000, (err) => {
     if (err) throw err
-    console.log('> Read on http://localhost:3000')
-  })
+    console.log('> Read on http://localhost:3000');
+  });
 })
